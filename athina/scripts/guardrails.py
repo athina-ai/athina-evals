7ea6f09c763a6760bef1a70344aff2d1d056bfc4
Vivek Aditya <vivek@athina.ai>
2024-03-30 15:02:21 +0530

    fixing cli and guardrails installation flow

--- athina/scripts/guardrails.py
+++ athina/scripts/guardrails.py
@@ -1,38 +1,39 @@
 import subprocess
 
-def main():
-    print("Installing guardrails")
+def install():
     try:
-        from guardrails import Guard
-    except Exception as e:
-        subprocess.check_call(["pip", "install", "-U", "guardrails-ai"])
-
-    try:
-        from guardrails.hub import GibberishText
-    except Exception as e:
-        subprocess.check_call(["guardrails", "hub", "install", "hub://guardrails/gibberish_text"])
-
-    try:
-        from guardrails.hub import SensitiveTopic
-    except Exception as e:
-        subprocess.check_call(["guardrails", "hub", "install", "hub://guardrails/sensitive_topics"])
-
-    try:
-        from guardrails.hub import NSFWText
-    except Exception as e:
-        subprocess.check_call(["guardrails", "hub", "install", "hub://guardrails/nsfw_text"])
-
-    try:
-        import nltk
-        import ssl
         try:
-            _create_unverified_https_context = ssl._create_unverified_context
-        except AttributeError:
-            pass
-        else:
-            ssl._create_default_https_context = _create_unverified_https_context
-        nltk.download('punkt')
+            from guardrails import Guard
+        except Exception as e:
+            subprocess.call(["pip", "install", "-U", "guardrails-ai==0.4.2"])
+
+        try:
+            from guardrails.hub import GibberishText
+        except Exception as e:
+            subprocess.call(["guardrails", "hub", "install", "hub://guardrails/gibberish_text"])
+
+        try:
+            from guardrails.hub import SensitiveTopic
+        except Exception as e:
+            subprocess.call(["guardrails", "hub", "install", "hub://guardrails/sensitive_topics"])
+
+        try:
+            from guardrails.hub import NSFWText
+        except Exception as e:
+            subprocess.call(["guardrails", "hub", "install", "hub://guardrails/nsfw_text"])
+
+        try:
+            import nltk
+            import ssl
+            try:
+                _create_unverified_https_context = ssl._create_unverified_context
+            except AttributeError:
+                pass
+            else:
+                ssl._create_default_https_context = _create_unverified_https_context
+            nltk.download('punkt')
+        except Exception as e:
+            print(f"Error: {e}")
     except Exception as e:
         print(f"Error: {e}")
-
-    print("Done installing guardrails")
+    
