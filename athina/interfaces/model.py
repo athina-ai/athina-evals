1c64c5900feea13484c88a0e958f424aba37644b
Vivek Aditya <vivek@athina.ai>
2024-04-13 15:03:10 +0530

    added newer models

--- athina/interfaces/model.py
+++ athina/interfaces/model.py
@@ -11,6 +11,7 @@ class Model(Enum):
     GPT4 = "gpt-4"
     GPT4_1106_PREVIEW = "gpt-4-1106-preview"
     GPT4_TURBO_PREVIEW = "gpt-4-turbo-preview"
+    GPT4_TURBO = "gpt-4-turbo"
     GPT35_TURBO_16K = "gpt-3.5-turbo-16k"
     COMMAND_LIGHT = "command-light"
     COMMAND = "command"
@@ -20,8 +21,14 @@ class Model(Enum):
     AZURE_GPT4_1106_PREVIEW = "azure/gpt-4-1106-preview"
     GEMINI_PROD = "gemini/gemini-prod"
     CLAUDE_2 = "claude-2"
+    CLAUDE_21 = "claude-2.1"
+    CLAUDE_3_HAIKU_20240307 = "claude-3-haiku-20240307"
+    CLAUDE_3_OPUS_20240229 = "claude-3-opus-20240229"
+    CLAUDE_3_SONNET_20240229 = "claude-3-sonnet-20240229"
     MISTRAL_TINY = "mistral/mistral-tiny"
     MISTRAL_SMALL = "mistral/mistral-small"
+    MISTRAL_MEDIUM = "mistral/mistral-medium"
+    MISTRAL_LARGE = "mistral/mistral-large"
 
     @staticmethod
     def is_supported(model_name: str) -> bool:
