6a31b822d18bd12246cd0f2fb6e1242ed51ff8f5
Vivek Aditya <vivek@athina.ai>
2024-04-16 09:25:38 +0530

    bug fixes for safety evals

--- athina/scripts/guardrails.py
+++ athina/scripts/guardrails.py
@@ -5,22 +5,30 @@ def install():
         try:
             from guardrails import Guard
         except Exception as e:
-            subprocess.call(["pip", "install", "-U", "guardrails-ai==0.4.2"])
+            result = subprocess.call(["pip", "install", "-U", "guardrails-ai==0.4.2"])
+            if result != 0:
+                print("Guardrails installation failed")
 
         try:
             from guardrails.hub import GibberishText
         except Exception as e:
-            subprocess.call(["guardrails", "hub", "install", "hub://guardrails/gibberish_text"])
+            result = subprocess.call(["guardrails", "hub", "install", "hub://guardrails/gibberish_text"])
+            if result != 0:
+                print("Guardrails installation failed. Ensure have the latest version of pip installed")
 
         try:
             from guardrails.hub import SensitiveTopic
         except Exception as e:
-            subprocess.call(["guardrails", "hub", "install", "hub://guardrails/sensitive_topics"])
+            result = subprocess.call(["guardrails", "hub", "install", "hub://guardrails/sensitive_topics"])
+            if result != 0:
+                print("Guardrails installation failed. Ensure have the latest version of pip installed")
 
         try:
             from guardrails.hub import NSFWText
         except Exception as e:
-            subprocess.call(["guardrails", "hub", "install", "hub://guardrails/nsfw_text"])
+            result = subprocess.call(["guardrails", "hub", "install", "hub://guardrails/nsfw_text"])
+            if result != 0:
+                print("Guardrails installation failed. Ensure have the latest version of pip installed")
 
         try:
             import nltk
