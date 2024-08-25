8eeb2dcd107da7a2d7e909e365b3bcb370fc5081
Vivek Aditya <vivek@athina.ai>
2024-08-25 19:29:29 +0530

    ATH 1998 fixed

--- athina/evals/llm/custom_prompt/evaluator.py
+++ athina/evals/llm/custom_prompt/evaluator.py
@@ -11,6 +11,7 @@ from ..example import FewShotExample
 from athina.interfaces.result import EvalResult, EvalResultMetric
 from athina.metrics.metric_type import MetricType
 
+
 class CustomPrompt(LlmEvaluator):
     """
     This evaluator can be configured with custom examples and instructions.
@@ -27,14 +28,14 @@ class CustomPrompt(LlmEvaluator):
     def __init__(
         self,
         eval_prompt: str,
-        output_type: str = 'boolean',
+        output_type: str = "boolean",
         display_name: str = None,
         metric_ids: List[str] = None,
         model: str = None,
         required_args: List[str] = [],
         examples: List[FewShotExample] = [],
         llm_service: Optional[AbstractLlmService] = None,
-        **kwargs
+        **kwargs,
     ):
         if eval_prompt is None:
             raise ValueError("eval_prompt is not defined")
@@ -48,9 +49,17 @@ class CustomPrompt(LlmEvaluator):
         self._model = model
         self._required_args = required_args
         self._examples = examples
-        self._system_message_template = (
-            self.DEFAULT_SYSTEM_MESSAGE_TEMPLATE + self.RETURN_FORMAT_INSTRUCTIONS
-        )
+        self._system_message_template = None
+
+        prompt_messages = kwargs.get("prompt_messages", [])
+        if (
+            prompt_messages
+            and len(prompt_messages) > 0
+            and prompt_messages[0].get("role") == "system"
+            and prompt_messages[0].get("content")
+            and prompt_messages[0]["content"].strip()
+        ):
+            self._system_message_template = prompt_messages[0]["content"]
 
         super().__init__(
             model=self._model,
@@ -59,11 +68,11 @@ class CustomPrompt(LlmEvaluator):
             llm_service=llm_service,
             **kwargs,
         )
-         # Create a custom Jinja2 environment with double curly brace delimiters and PreserveUndefined
+        # Create a custom Jinja2 environment with double curly brace delimiters and PreserveUndefined
         self.env = Environment(
-            variable_start_string='{{',
-            variable_end_string='}}',
-            undefined=PreserveUndefined
+            variable_start_string="{{",
+            variable_end_string="}}",
+            undefined=PreserveUndefined,
         )
 
     @property
@@ -89,7 +98,7 @@ class CustomPrompt(LlmEvaluator):
     @property
     def examples(self):
         return self._examples
-    
+
     def to_config(self) -> Optional[Dict]:
         return {
             "eval_prompt": self._eval_prompt,
@@ -103,24 +112,27 @@ class CustomPrompt(LlmEvaluator):
         return template.render(**kwargs)
 
     def _system_message(self) -> str:
-        if self._output_type == 'boolean':
-            return (
-                "### INSTRUCTIONS ###\n"
-                "You are an expert at evaluating responses by an AI.\n"
-                "Based on the instructions provided, you will evaluate the response and determine if it passes or fails.\n"
-                "You MUST return a JSON object with the following fields:\n"
-                "- result: Result must be either 'Pass' or 'Fail'.\n"
-                "- explanation: An explanation of why the result is Pass or Fail.\n"
-            )
-        elif self._output_type == 'numeric':
-            return (
-                "### INSTRUCTIONS ###\n"
-                "You are an expert at evaluating responses by an AI.\n"
-                "Based on the instructions provided, you will evaluate the response and provide a score.\n"
-                "You MUST return a JSON object with the following fields:\n"
-                "- score: The score based on the provided grading criteria.\n"
-                "- explanation: An explanation of the score.\n"
-            )
+        if self._system_message_template:
+            return self._system_message_template
+        else:
+            if self._output_type == "boolean":
+                return (
+                    "### INSTRUCTIONS ###\n"
+                    "You are an expert at evaluating responses by an AI.\n"
+                    "Based on the instructions provided, you will evaluate the response and determine if it passes or fails.\n"
+                    "You MUST return a JSON object with the following fields:\n"
+                    "- result: Result must be either 'Pass' or 'Fail'.\n"
+                    "- explanation: An explanation of why the result is Pass or Fail.\n"
+                )
+            elif self._output_type == "numeric":
+                return (
+                    "### INSTRUCTIONS ###\n"
+                    "You are an expert at evaluating responses by an AI.\n"
+                    "Based on the instructions provided, you will evaluate the response and provide a score.\n"
+                    "You MUST return a JSON object with the following fields:\n"
+                    "- score: The score based on the provided grading criteria.\n"
+                    "- explanation: An explanation of the score.\n"
+                )
 
     def _evaluate(self, **kwargs) -> EvalResult:
         """
@@ -133,23 +145,24 @@ class CustomPrompt(LlmEvaluator):
         # Construct Prompt
         messages = self._prompt_messages(**kwargs)
 
-        # Run the LLM Completion
-
-        chat_completion_response_json: dict = self.llm_service.json_completion(
-            model=self._model,
-            messages=messages,
-            temperature=self.TEMPERATURE,
-        )
-    
         metrics = []
         try:
-            if self._output_type == 'boolean':
+            # Run the LLM Completion
+            chat_completion_response_json: dict = self.llm_service.json_completion(
+                model=self._model,
+                messages=messages,
+                temperature=self.TEMPERATURE,
+            )
+
+            if self._output_type == "boolean":
                 result = chat_completion_response_json["result"]
                 explanation = chat_completion_response_json["explanation"]
                 failure = self.is_failure(result)
                 passed_value = 1 - float(failure)
-                metrics.append(EvalResultMetric(id=MetricType.PASSED.value, value=passed_value))
-            elif self._output_type == 'numeric':
+                metrics.append(
+                    EvalResultMetric(id=MetricType.PASSED.value, value=passed_value)
+                )
+            elif self._output_type == "numeric":
                 score = chat_completion_response_json["score"]
                 explanation = chat_completion_response_json["explanation"]
                 metrics.append(EvalResultMetric(id=MetricType.SCORE.value, value=score))
@@ -157,7 +170,10 @@ class CustomPrompt(LlmEvaluator):
 
         except Exception as e:
             logger.error(f"Error occurred during eval: {e}")
-            raise e
+            if isinstance(e, ValueError):
+                raise ValueError("LLM evals must return a result/score and explanation. The LLM response did not return the correct structure for parsing evaluation results.")
+            else:
+                raise e
 
         end_time = time.time()
         eval_runtime_ms = int((end_time - start_time) * 1000)
@@ -172,4 +188,3 @@ class CustomPrompt(LlmEvaluator):
             metrics=metrics,
         )
         return {k: v for k, v in llm_eval_result.items() if v is not None}
-
